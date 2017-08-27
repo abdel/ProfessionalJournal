d365bba3ffada1132819a58ed327e5378aa68cd3
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-08-28 00:56:07 +1000

    Use loading indicator in RegisterPage, add some comments

--- Views/RegisterPage.xaml.cs
+++ Views/RegisterPage.xaml.cs
@@ -5,7 +5,7 @@ using ProfessionalJournal.Models;
 using Microsoft.WindowsAzure.MobileServices;
 
 using Xamarin.Forms;
-
+using Acr.UserDialogs;
 
 namespace ProfessionalJournal
 {
@@ -13,6 +13,9 @@ namespace ProfessionalJournal
     {
         MobileServiceClient client;
 
+        /// <summary>
+        /// Constructor for RegisterPage class that sets up the MobileServiceClient
+        /// </summary>
         public RegisterPage()
         {
             InitializeComponent();
@@ -20,12 +23,17 @@ namespace ProfessionalJournal
             this.client = new MobileServiceClient(Constants.ApplicationURL);
         }
 
-        // Data methods
+        /// <summary>
+        /// Registers a new author by sending a request to the /api/register
+        /// endpoint
+        /// </summary>
+        /// <param name="author">An Author model object</param>
         async Task RegisterAuthor(Author author)
         {
             try
             {
-                var result = await client.InvokeApiAsync<Author, AuthorResponse>("register", author);
+                // Send request to POST /api/register endpoint
+                var result = await client.InvokeApiAsync<Author, string>("register", author);
 
                 await DisplayAlert("Success", "You have been registered as an author.", "OK");
             }
@@ -33,31 +41,38 @@ namespace ProfessionalJournal
             {
                 await DisplayAlert("Error", e.Message, "OK");
             }
-           
         }
 
+
+        /// <summary>
+        /// Triggered by clicking the register button on RegisterPage to
+        /// create new authors
+        /// </summary>
         public async void OnRegister(object sender, EventArgs e)
         {
-            var author = new Author { 
+            // Show loading indicator
+            UserDialogs.Instance.ShowLoading("Loading", MaskType.Black);
+
+            // New author object
+            var author = new Author
+            {
                 Username = newAuthorUsername.Text.ToLower(),
                 Password = AppHelper.GeneratePasswordHash(
                     newAuthorPassword.Text
                 )
             };
 
+            // Register author
             await RegisterAuthor(author);
 
+            // Hide loading indicator
+            UserDialogs.Instance.HideLoading();
+
+            // Clear the register form
             newAuthorUsername.Text = string.Empty;
             newAuthorPassword.Text = string.Empty;
             newAuthorUsername.Unfocus();
             newAuthorPassword.Unfocus();
         }
     }
-
-    public class AuthorResponse
-    {
-        public string Id { get; set; }
-    }
-
-
 }
