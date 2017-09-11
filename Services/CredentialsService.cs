443497bdfa684ad9bb3be012c95a38bc95ff52e5
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-09-11 11:34:16 +1000

    Add CredentialsStore using Xamarin.Auth

---
+++ Services/CredentialsService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Auth;
+
+namespace ProfessionalJournal
+{
+
+    public class CredentialsService : ICredentialsService
+    {
+        public string Username
+        {
+            get
+            {
+                var account = AccountStore.Create().FindAccountsForService(App.AppName).FirstOrDefault();
+                return (account != null) ? account.Username : null;
+            }
+        }
+
+        public string Token
+        {
+            get
+            {
+                var account = AccountStore.Create().FindAccountsForService(App.AppName).FirstOrDefault();
+                return (account != null) ? account.Properties["Token"] : null;
+            }
+        }
+
+        public void SaveCredentials(string userName, string token)
+        {
+            if (!string.IsNullOrWhiteSpace(userName) && !string.IsNullOrWhiteSpace(token))
+            {
+                Account account = new Account
+                {
+                    Username = userName
+                };
+                account.Properties.Add("Token", token);
+                AccountStore.Create().Save(account, App.AppName);
+            }
+
+        }
+
+        public void DeleteCredentials()
+        {
+            var account = AccountStore.Create().FindAccountsForService(App.AppName).FirstOrDefault();
+            if (account != null)
+            {
+                AccountStore.Create().Delete(account, App.AppName);
+            }
+        }
+
+        public bool DoCredentialsExist()
+        {
+            return AccountStore.Create().FindAccountsForService(App.AppName).Any() ? true : false;
+        }
+    }
+}
---
+++ Services/ICredentialsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProfessionalJournal
+{
+    public interface ICredentialsService
+    {
+		string Username { get; }
+
+		string Token { get; }
+
+		void SaveCredentials(string userName, string token);
+
+		void DeleteCredentials();
+
+		bool DoCredentialsExist();
+    }
+}
