e2832a2ccd967f86c04ecb42e51bc226f3e21a15
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-09-22 14:45:18 +1000

    Fix issue in nav bar renderer

--- iOS/JournalsPageRenderer.cs
+++ iOS/JournalsPageRenderer.cs
@@ -21,34 +21,35 @@ namespace ProfessionalJournal.iOS
 
         public override void ViewWillAppear(bool animated)
         {
-        	base.ViewWillAppear(animated);
+            base.ViewWillAppear(animated);
 
-        	var LeftNavList = new List<UIBarButtonItem>();
-        	var rightNavList = new List<UIBarButtonItem>();
+            var LeftNavList = new List<UIBarButtonItem>();
+            var rightNavList = new List<UIBarButtonItem>();
 
-        	var navigationItem = this.NavigationController.TopViewController.NavigationItem;
+            var navigationItem = this.NavigationController.TopViewController.NavigationItem;
 
-        	for (var i = 0; i < Element.ToolbarItems.Count; i++)
-        	{
+            if (navigationItem.RightBarButtonItems.Length < 2)
+                return;
+            
+            for (var i = 0; i < Element.ToolbarItems.Count; i++)
+            {
+                var reorder = (Element.ToolbarItems.Count - 1);
+                var ItemPriority = Element.ToolbarItems[reorder - i].Priority;
 
-        		var reorder = (Element.ToolbarItems.Count - 1);
-        		var ItemPriority = Element.ToolbarItems[reorder - i].Priority;
+                if (ItemPriority == 1)
+                {
+                    UIBarButtonItem LeftNavItems = navigationItem.RightBarButtonItems[i];
+                    LeftNavList.Add(LeftNavItems);
+                }
+                else if (ItemPriority == 0)
+                {
+                    UIBarButtonItem RightNavItems = navigationItem.RightBarButtonItems[i];
+                    rightNavList.Add(RightNavItems);
+                }
+            }
 
-        		if (ItemPriority == 1)
-        		{
-        			UIBarButtonItem LeftNavItems = navigationItem.RightBarButtonItems[i];
-        			LeftNavList.Add(LeftNavItems);
-        		}
-        		else if (ItemPriority == 0)
-        		{
-        			UIBarButtonItem RightNavItems = navigationItem.RightBarButtonItems[i];
-        			rightNavList.Add(RightNavItems);
-        		}
-        	}
-
-        	navigationItem.SetLeftBarButtonItems(LeftNavList.ToArray(), false);
-        	navigationItem.SetRightBarButtonItems(rightNavList.ToArray(), false);
-
+            navigationItem.SetLeftBarButtonItems(LeftNavList.ToArray(), false);
+            navigationItem.SetRightBarButtonItems(rightNavList.ToArray(), false);
         }
     }
 }
