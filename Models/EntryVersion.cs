2844a702f6bc17121bb55ecb9a6ad321815d9478
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-09-22 14:45:05 +1000

    Add EntryVersion model

---
+++ Models/EntryVersion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace ProfessionalJournal
+{
+    public class EntryVersion
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+		[JsonProperty("entry_id")]
+		public string EntryId { get; set; }
+
+		[JsonProperty("version_track_id")]
+		public string VersionTrackId { get; set; }
+
+        [JsonProperty("text_Entry")]
+        public string TextEntry { get; set; }
+        
+        [JsonProperty("attachment")]
+        public string Attachment { get; set; }
+
+        [JsonProperty("modify_reason")]
+        public string ModifyReason { get; set; }
+    }
+}
